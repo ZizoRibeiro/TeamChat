ab2fc6d7b2446d88d33bd3952babe346f38f456f
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-06-29 14:24:12 -0700

    Implemented action cable for messages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :talks, only: [:show]
   resources :team_users, only: [:create, :destroy]
   devise_for :users, :controllers => { registrations: 'registrations'}
-
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
